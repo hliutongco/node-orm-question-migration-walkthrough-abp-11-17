b6c2cda1e843628f53a2863313b246de77210010
Helen Liutongco <bellpickle@gmail.com>
2017-11-16 20:47:36 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -3,8 +3,24 @@
 const db = require("../config/db")
 
 class Question{
-
+  static CreateTable(){
+    const sql=`
+    CREATE TABLE IF NOT EXISTS questions (
+      id INTEGER PRIMARY KEY,
+      content TEXT
+    )
+    `
+    return new Promise(function(resolve){
+    db.run(sql,function(){
+      resolve("questions table created")
+    })
+  })
+  }
 }
+
+(async function(){
+  await CreateTable();
+})();
 
 
 module.exports = Question;
